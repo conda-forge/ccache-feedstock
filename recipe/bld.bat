e98cdffc47eb26b334869ce17f38957637b292a1
Ray Donnelly <mingw.android@gmail.com>
2020-03-17 16:06:54 +0100

    Windows WIP

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,10 @@
+if "%DEBUG_C%" == "" (
+  set BT=Release
+) else (
+  set BT=Debug
+)
 cmake -GNinja ^
+      -DCMAKE_BUILD_TYPE=%BT% ^
+      -DCMAKE_VERBOSE_MAKEFILE=ON ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_BIN% .
-ninja install
+ninja -v install
