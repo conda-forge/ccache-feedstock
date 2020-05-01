31a49d73d20a86c6b9cf850432eab09a0496cf57
Ray Donnelly <mingw.android@gmail.com>
2020-05-02 00:36:29 +0200

    Use upstream for Windows too, update to 3.7.9

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,26 @@
 
-if "%DEBUG_C%" == "" (
-  set BT=Release
-) else (
-  set BT=Debug
-)
-cmake -GNinja ^
-      -DCMAKE_BUILD_TYPE=%BT% ^
-      -DCMAKE_VERBOSE_MAKEFILE=ON ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_BIN% ccache.cristianadam
-ninja -v install
+:: if "%DEBUG_C%" == "" (
+::   set BT=Release
+:: ) else (
+::   set BT=Debug
+:: )
+:: cmake -GNinja ^
+::       -DCMAKE_BUILD_TYPE=%BT% ^
+::       -DCMAKE_VERBOSE_MAKEFILE=ON ^
+::       -DCMAKE_INSTALL_PREFIX=%LIBRARY_BIN% ccache.christianadam
+:: ninja -v install
+
+copy "%RECIPE_DIR%\build.sh" .
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+bash -lc ./build.sh
+if errorlevel 1 exit 1
+exit 0
