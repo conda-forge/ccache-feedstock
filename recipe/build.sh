7483d7bee47fc30cab9ed76d018787277bd0d67f
Rosen Matev <rosen.matev@cern.ch>
2020-11-28 18:35:56 +0100

    Workaround for osx_arm64 build, see ccache/ccache#734

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,12 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 mkdir build
 cd build
 
+if [ "$target_platform" = "osx-arm64" ]; then
+    # Workaround for wrong detection of vector extensions,
+    # see https://github.com/ccache/ccache/issues/734
+    CMAKE_ARGS="${CMAKE_ARGS} -DHAVE_AVX2=NO -DHAVE_C_AVX2=NO -DHAVE_C_AVX512=NO -DHAVE_C_SSE2=NO -DHAVE_C_SSE41=NO"
+fi
+
 cmake \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
