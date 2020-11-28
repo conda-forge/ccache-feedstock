516c6d709f93c94472135ba8877447f1ba68ec13
Rosen Matev <rosen.matev@cern.ch>
2020-11-28 15:23:04 +0100

    Update build to use cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
-./configure --prefix=$PREFIX
+
+mkdir build
+cd build
+
+cmake \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    ${SRC_DIR}
+
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+    make check
 fi
 make install
