cc977c0839e805b7782564cbd942591c984cbf2f
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-10-20 10:19:18 +0200

    try mingw64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,9 @@ cd build
 
 cmake -GNinja ^
     %CMAKE_ARGS% ^
-    %SRC_DIR%
+    %SRC_DIR% ^
+    -DHAVE_CXX_ATOMICS64_WITHOUT_LIB=True ^
+    -DHAVE_CXX_ATOMICS_WITHOUT_LIB=True
 
 ninja
 ninja install
