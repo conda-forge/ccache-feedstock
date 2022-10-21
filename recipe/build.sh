af550224f5b1daf429acef8ad7997ddae6f59bd3
Keith Kraus <keith.j.kraus@gmail.com>
2022-10-21 16:02:49 -0400

    update build script and dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,14 @@
 mkdir build
 cd build
 
-if [ "$target_platform" = "osx-arm64" ]; then
-    # Workaround for wrong detection of vector extensions,
-    # see https://github.com/ccache/ccache/issues/734
-    CMAKE_ARGS="${CMAKE_ARGS} -DHAVE_AVX2=NO -DHAVE_C_AVX2=NO -DHAVE_C_AVX512=NO -DHAVE_C_SSE2=NO -DHAVE_C_SSE41=NO"
+#if [ "$target_platform" = "osx-arm64" ]; then
+#    # Workaround for wrong detection of vector extensions,
+#    # see https://github.com/ccache/ccache/issues/734
+#    CMAKE_ARGS="${CMAKE_ARGS} -DHAVE_AVX2=NO -DHAVE_C_AVX2=NO -DHAVE_C_AVX512=NO -DHAVE_C_SSE2=NO -DHAVE_C_SSE41=NO"
+#fi
+
+if [ "$target_platform" = "osx-64" ]; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
 cmake \
